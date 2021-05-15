b3aa591776fa3f4ba91ab79305fc64ec8a161909
hp <hp@LAPTOP-VD5LIJ9S>
2021-05-15 18:09:27 +0300

    Human Resource Management System with Spring Boot and PostgreSql

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employers.java
@@ -0,0 +1,62 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employers")
+public class Employers {
+	@Id
+	@GeneratedValue
+	@Column(name = "company_name")
+	private String companyName;
+	
+	@Column(name = "web_address")
+	private String webAddress;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	
+	public Employers () {
+		
+	}
+	
+	public Employers(String companyName, String webAddress, String phoneNumber) {
+		super();
+		this.companyName = companyName;
+		this.webAddress = webAddress;
+		this.phoneNumber = phoneNumber;
+		
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getWebAddress() {
+		return webAddress;
+	}
+
+	public void setWebAddress(String webAddress) {
+		this.webAddress = webAddress;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	
+}
